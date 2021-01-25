3474d0e6f55b417ae953cf8cc0bead361da8bba1
l <codn3@outlook.com>
2021-01-25 13:13:33 +0000

    title, description, price, thumbnail, authors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Book = (props) => {
-      return (
+  //  let {id, volumeInfo: {title, authors, description}, saleInfo: {listPrice: {amount}}} = props.book;
+
+   const info = props.book.volumeInfo ;
+  // const { volumeInfo } = props.book;
+ const { volumeInfo : { title,  authors, description, imageLinks : {thumbnail}} } = props.book ;
+  
+ const renderAmount = ( ) => {
+     if (props.book.saleInfo && props.book.saleInfo.listPrice && props.book.saleInfo.listPrice.amount) {
+        return '£' + props.book.saleInfo.listPrice.amount ;
+     }
+ }
+
+   const renderAuthors = ( ) => {
+       if (authors.length === 1 ) {
+           return authors ;
+       }
+       return authors.map(author => author + ', ' ) ;
+   }
+     return (
             <div>
-                <h2>{props.book.volumeInfo.title}</h2>
+                <img src={thumbnail} />
+                <h2>{title} - {renderAuthors( ) }</h2>
+                <p>{renderAmount ( ) }</p>
+                <p>{description}</p>
             </div>
     );
 }
